7fb1f60a008e16c4f31d0b60cbb439a0aa972fd9
kungumasakthivel <kungumasakthivel.g2020aids@sece.ac.in>
2024-01-09 21:57:52 +0530

    Linear search folder
    file iterative method

---
+++ Linear Search/iterative.py
@@ -0,0 +1,13 @@
+def linear_search(l, x):
+    """
+    Where, l  -> is list of elements to be searched for finding element
+    x -> is the element to be searched
+    If element is not found then text "No element is found" will be returned
+    """
+
+    for i in range(len(l)):
+        if i == x:
+            return "Element found in index " + str(i)
+    return "Element not found"
+
+print(linear_search([1, 2, 3, 4], 3))
