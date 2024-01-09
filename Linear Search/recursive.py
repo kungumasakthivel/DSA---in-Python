a31cd7f1e8973a6b174281e589c8160ef9bfd1d8
kungumasakthivel <kungumasakthivel.g2020aids@sece.ac.in>
2024-01-09 22:26:30 +0530

    added recursive linear search

---
+++ Linear Search/recursive.py
@@ -0,0 +1,21 @@
+def linear_search(l, x, idx):
+    """
+    Where, l -> is list of elements to be searched
+           x -> is element to be searched
+           idx -> is total number of element
+    """
+    idx = idx - 1
+
+    if idx == 0:
+        return "Element not found"
+    elif x == l[idx]:
+        return "Element fouund at index " + str(idx)
+    else:
+        return linear_search(l, x, idx)
+
+l = [1, 2, 3, 11, 4, 9]
+x = 11
+idx = len(l)
+
+print(linear_search(l, x, idx))
+    
